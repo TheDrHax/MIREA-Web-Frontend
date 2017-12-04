e19126eee967a6884c1bd092b1056aaedac8983e
Дмитрий Карих <the.dr.hax@gmail.com>
2017-12-04 18:29:31 +0300

    Home: Use react-columns for Cards

--- modules/Home.js
+++ modules/Home.js
@@ -1,37 +1,50 @@
 import React from "react";
 
+import Columns from 'react-columns';
 import {
   Card, CardHeader, CardMedia, CardTitle
 } from 'material-ui/Card';
 
 import {Page} from './Page';
 
+const CardA = (
+  <Card style={{margin: 16}}>
+    <CardHeader
+      title="Doge"
+      subtitle="App maintainer"
+      avatar="images/doge_avatar.jpg"
+    />
+    <CardMedia>
+      <img src="images/doge.jpg" alt="" />
+    </CardMedia>
+    <CardTitle title="Wow" subtitle="Such design" />
+  </Card>
+)
+
+const CardB = (
+  <Card style={{margin: 16}}>
+    <CardHeader
+      title="Doge"
+      subtitle="App maintainer"
+      avatar="images/doge_avatar.jpg"
+    />
+    <CardMedia
+      overlay={<CardTitle title="Big overlay" subtitle="Woah" />}>
+      <img src="images/doge.jpg" style={{transform: 'scale(-1, 1)'}} />
+    </CardMedia>
+  </Card>
+)
+
 class Home extends Page {
   render() {
     return (
       <Page title="Doge Unlimited">
-        <Card style={{margin: 16, width: 400}}>
-          <CardHeader
-            title="Doge"
-            subtitle="App maintainer"
-            avatar="images/doge_avatar.jpg"
-          />
-          <CardMedia>
-            <img src="images/doge.jpg" alt="" />
-          </CardMedia>
-          <CardTitle title="Wow" subtitle="Such design" />
-        </Card>
-        <Card style={{margin: 16, width: 400}}>
-          <CardHeader
-            title="Doge"
-            subtitle="App maintainer"
-            avatar="images/doge_avatar.jpg"
-          />
-          <CardMedia
-            overlay={<CardTitle title="Big overlay" subtitle="Woah" />}>
-            <img src="images/doge.jpg" style={{transform: 'scale(-1, 1)'}} />
-          </CardMedia>
-        </Card>
+        <Columns columns={2}>
+          {CardA}
+          {CardB}
+          {CardB}
+          {CardA}
+        </Columns>
       </Page>
     )
   }
