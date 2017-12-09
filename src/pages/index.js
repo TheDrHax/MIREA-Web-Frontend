cbbb3d18b7803b2a0e9e14d13cafc3a6bbdf5cd9
Дмитрий Карих <the.dr.hax@gmail.com>
2017-12-09 19:58:06 +0300

    Load branch list from mosmetro.duckdns.org
    This requires CORS override in development version but should work on
    GitHub Pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,67 @@
 import React from "react";
+import fetch from "node-fetch";
+import {withPrefix} from "gatsby-link";
+
+import {Card, CardHeader} from 'material-ui/Card';
 
 import Page from '../components/Page';
 
-export default () => (
-  <Page title="Главная страница">
-    "Wow. Such emptiness"
-  </Page>
-)
+class Branch extends React.Component {
+	render() {
+		return (
+			<Card style={{margin: 16}}>
+				<CardHeader
+					title={this.props.data.name}
+					subtitle={
+						this.props.data.by_build == "1" ?
+							"Сборка #" + this.props.data.build :
+							"Версия #" + this.props.data.version
+					}
+				 	avatar={withPrefix(
+						this.props.data.by_build == "1" ?
+							"/images/jenkins.png" : "/images/github.png"
+					)} />
+			</Card>
+		)
+	}
+}
+
+export default class Main extends React.Component {
+	state = {
+		loaded: false
+	}
+
+	updateData(json) {
+		var data = []
+		for (var branch in json) {
+			data.push(json[branch])
+		}
+
+		this.setState({
+			loaded: true,
+			data: data
+		})
+	}
+
+  renderBanches() {
+		return (
+			this.state.data.map((data) => (
+				<Branch data={data} key={data.name} />
+			))
+		)
+	}
+
+	render() {
+		if (!this.state.loaded) {
+			fetch("https://mosmetro.duckdns.org/api/v1/branches.php")
+				.then(res => res.json())
+				.then(json => this.updateData(json))
+		}
+
+		return (
+			<Page title="Главная страница">
+				{this.state.loaded ? this.renderBanches() : "Loading..."}
+		  </Page>
+		)
+	}
+}
